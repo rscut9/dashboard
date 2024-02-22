f63c71f09faa66890421640f8b534d5ab4af3c13
rscut9 <rscutaru567@gmail.com>
2024-02-22 12:15:07 +0100

    front

--- dashboard_unir/dashboard_app/urls.py
+++ dashboard_unir/dashboard_app/urls.py
@@ -3,4 +3,8 @@ from .views import *
 
 urlpatterns = [
     path('', home, name='home'),
+    path('logout/', exit, name='exit'),
+    path('ver/', ver_coleccion, name='ver_coleccion'),
+    path('mostrar/', mostrar_coleccion, name='mostrar_coleccion'),
+    path('crear_usuario/', create_user, name='crear_usuario'),
 ]
--- dashboard_unir/dashboard_app/views.py
+++ dashboard_unir/dashboard_app/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
+from pymongo import MongoClient
 
-# Create your views here.
+# Conexión a la base de datos MongoDB
+client = MongoClient('mongodb://localhost:27017')
+mydatabase = client.dashboard
 
 def home(request):
     return render(request, 'home.html')
+
+def exit(request):
+    return render(request, 'home.html')
+
+def ver_coleccion(request):
+    # Obtiene la lista de colecciones y las ordena alfabéticamente
+    colecciones = sorted(mydatabase.list_collection_names())
+
+    # Renderiza la página 'ver_coleccion' con la lista de colecciones
+    return render(request, 'ver_coleccion.html', {'colecciones': colecciones})
+
+def mostrar_coleccion(request):
+    return render(request, 'detalle_coleccion.html')
+
+def create_user(request):
+    return render(request, 'crear_usuario.html')
