7c1bd61b06327fba1950fdd0b81bae04dd71bc76
rscut9 <rscutaru567@gmail.com>
2024-02-22 10:09:11 +0100

    primera vista

--- dashboard_unir/dashboard_unir/settings.py
+++ dashboard_unir/dashboard_unir/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'dashboard_app.apps.DashboardAppConfig',
 ]
 
 MIDDLEWARE = [
